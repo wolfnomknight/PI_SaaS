2273fb7f624982b4ff50808c571c1508f09e820d
Arthur Peixoto Melo <arthurpeixotomelo@gmail.com>
2023-10-17 23:42:40 -0300

    update main

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="src/templates")
 
 
-# @app.get("/", response_class=HTMLResponse)
-# def main(request: Request):
-#     return Jinja2Templates(directory="src/templates").TemplateResponse(
-#         "index.html", {"request": request}
-#     )
 @app.get("/", response_class=HTMLResponse)
-def main():
-    html = """<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>PI</title>
-</head>
-<body>
-  <div>hello world</div>
-</body>
-</html>"""
-    return html
+def main(request: Request):
+    return templates.TemplateResponse("index.html", context={"request": request})
