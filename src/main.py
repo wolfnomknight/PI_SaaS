c6f46947111ad310deccb621e674665b25742d94
Arthur Peixoto Melo <arthurpeixotomelo@gmail.com>
2023-10-17 22:22:26 -0300

    update deploy

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+def homepage(request: Request):
+    return Jinja2Templates(directory="src/templates").TemplateResponse(
+        "index.html", {"request": request}
+    )
