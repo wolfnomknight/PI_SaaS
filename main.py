540d0426a3d3a91270d0a3357f280b1c992792e4
Arthur Peixoto Melo <arthurpeixotomelo@gmail.com>
2024-05-07 23:27:55 -0300

    pi 03

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
+from os import listdir, getcwd
+from contextlib import asynccontextmanager
+
+import pandas as pd
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
-templates = Jinja2Templates(directory="src/templates")
+plants = {'excaulebur': None, 'totosa': None}
 
+def get_plant_data(plant):
+    df = pd.concat([get_file_data(file, plant) for file in listdir(f'{getcwd()}\\data')])
+    return df.sort_values(by=['Data', 'Hora']).to_json(orient='records')
 
-@app.get("/", response_class=HTMLResponse)
-def main(request: Request):
-    return templates.TemplateResponse("index.html", context={"request": request})
+def get_file_data(file, plant):
+    if file.split('_')[0] == plant.title():
+        df = pd.read_csv(f'{getcwd()}\\data\\{file}', encoding='unicode_escape', engine='python')
+        df['Data'] = file.split('.')[0][-10:]
+        return df[['Valor do sinal', 'Temperatura', 'Umidade', 'Data', 'Hora']]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    for plant in plants.keys():
+        plants[plant] = get_plant_data(plant)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+templates = Jinja2Templates(directory='templates')
+
+@app.get('/', response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(request=request, name='index.html')
+
+@app.get('/plants/{plant}')
+def get_plant(plant):
+    return plants.get(plant)
