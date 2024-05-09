22d58c679c0b4d52fc7975817c1c0979076f4384
Arthur Peixoto Melo <arthurpeixotomelo@gmail.com>
2024-05-08 22:06:11 -0300

    load only one csv

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ from fastapi.templating import Jinja2Templates
 
 plants = {'excaulebur': None, 'totosa': None}
 
-def get_plant_data(plant):
-    df = pd.concat([get_file_data(file, plant) for file in listdir(join(dirname(abspath(__file__)), 'data'))])
-    return df.sort_values(by=['Data', 'Hora']).to_json(orient='records')
+# def get_plant_data(plant):
+#     df = pd.concat([get_file_data(file, plant) for file in listdir(join(dirname(abspath(__file__)), 'data'))])
+#     return df.sort_values(by=['Data', 'Hora']).to_json(orient='records')
 
-def get_file_data(file, plant):
-    if file.split('_')[0] == plant.title():
-        with open(join(dirname(abspath(__file__)), 'data', file), 'r') as f:
-            df = pd.read_csv(f, encoding='unicode_escape', engine='python')
-            df['Data'] = file.split('.')[0][-10:]
-            return df[['Valor do sinal', 'Temperatura', 'Umidade', 'Data', 'Hora']]
+# def get_file_data(file, plant):
+#     if file.split('_')[0] == plant.title():
+#         with open(join(dirname(abspath(__file__)), 'data', file), 'r') as f:
+#             df = pd.read_csv(f, encoding='unicode_escape', engine='python')
+#             df['Data'] = file.split('.')[0][-10:]
+#             return df[['Valor do sinal', 'Temperatura', 'Umidade', 'Data', 'Hora']]
 
 # @asynccontextmanager
 # async def lifespan(app: FastAPI):
@@ -33,7 +33,7 @@ app = FastAPI()
 @app.on_event("startup")
 def startup_event():
     for plant in plants.keys():
-        plants[plant] = get_plant_data(plant)
+        plants[plant] = pd.read_csv(join(dirname(abspath(__file__))), 'data', f'{plant.title()}.csv', encoding='unicode_escape', engine='python').sort_values(by=['Data', 'Hora']).to_json(orient='records')
 
 templates = Jinja2Templates(directory='templates')
 
@@ -52,10 +52,10 @@ def test():
 def get_plant(plant):
     return plants.get(plant)
 
-@app.get('/plants/{plant}/data')
-def get_data(plant):
-    return get_plant_data(plant)
+# @app.get('/plants/{plant}/data')
+# def get_data(plant):
+#     return get_plant_data(plant)
 
-@app.get('/plants/{plant}/{file}')
-def get_data(plant, file):
-    return get_file_data(file, plant)
+# @app.get('/plants/{plant}/{file}')
+# def get_data(plant, file):
+#     return get_file_data(file, plant)
