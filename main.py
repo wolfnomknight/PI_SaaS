f7c8b5bb62fc5177361048a82954e5236e0b4277
Arthur Peixoto Melo <arthurpeixotomelo@gmail.com>
2024-05-08 22:10:07 -0300

    load csv in requests

--- main.py
+++ main.py
@@ -50,7 +50,9 @@ def test():
 
 @app.get('/plants/{plant}')
 def get_plant(plant):
-    return plants.get(plant)
+    return pd.read_csv(join(dirname(abspath(__file__))), 'data', f'{plant.title()}.csv', encoding='unicode_escape', engine='python').sort_values(by=['Data', 'Hora']).to_json(orient='records')
+
+# plants.get(plant)
 
 # @app.get('/plants/{plant}/data')
 # def get_data(plant):
